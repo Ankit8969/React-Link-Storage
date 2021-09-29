25d3c2d236e00bac32e41c9ab26d4b1d5065da5e
Ankit Kumar <47697582+Ankit8969@users.noreply.github.com>
2021-09-29 15:13:01 +0530

    Add files via upload

---
+++ src/components/footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>❤️ Created By Ankit ❤️</p>
+      <p>
+        <a
+          href="https://www.linkedin.com/in/ankit-kumar-938646180/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fab fa-linkedin-in" />
+        </a>
+        <a href="https://github.com/Ankit8969" target="_blank" rel="noreferrer">
+          <i className="fab fa-github" style={{ marginLeft: "10px" }} />
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
